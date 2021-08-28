a4ffd687aedb522cb304597d6c656c8143d36ac1
Daniel Harper <daniel.c.harper@live.com>
2021-08-28 12:29:41 -0600

    Initial Commit

---
+++ build/CMakeFiles/modular.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/modular.dir/main.cpp.o"
+  "modular.pdb"
+  "modular"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/modular.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
